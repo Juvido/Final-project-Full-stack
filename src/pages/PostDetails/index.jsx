f89056167a7e21673ba3abd68ad8033a23df9e14
juliana <julianavido@outlook.com>
2023-05-16 16:11:15 -0700

    delete route

--- src/pages/PostDetails/index.jsx
+++ src/pages/PostDetails/index.jsx
@@ -1,15 +1,18 @@
 import { api } from "../../api/api.js";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { useParams, Link } from "react-router-dom";
-
+import { AuthContext } from "../../contexts/authContext.jsx";
 import { useNavigate } from "react-router-dom";
+import { ConfirmationButton } from "../../components/ConfirmationButton/index.jsx";
 
 export function PostDetails() {
   const params = useParams();
   const [post, setPost] = useState({
     comments: [],
   });
-  //const navigate = useNavigate();
+  const { loggedInUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [ deleteStatus, setDeleteStatus ] = useState(false);
 
   useEffect(() => {
     async function fetchPost() {
@@ -24,6 +27,17 @@ export function PostDetails() {
     fetchPost();
   }, []);
 
+  async function handleDelete(commentId, currentIndex) {
+    try {
+      await api.delete(`/comment/${commentId}`);
+      setPost((currentState)=> {
+        currentState.comments.splice(currentIndex, 1)
+        return {...currentState }
+      })
+    } catch (e) {
+      console.log(e);
+    }
+  }
   return (
     <>
       <h2>{post.name} </h2>
@@ -34,14 +48,25 @@ export function PostDetails() {
       <Link to={`/comentarios/${params.id}`}>
         <button> Comentar</button>
       </Link>
-      <h3> Comentarios</h3>
+      <h3> Comentarios: </h3>
 
       {post.comments.length ? (
         <ul>
-          {post.comments.map((currentComment) => {
+          {post.comments.map((currentComment, currentIndex) => {
             return (
               <>
                 <li> {currentComment.text}</li>
+                {currentComment.creator === loggedInUser.user._id ||
+                post.creator === loggedInUser.user._id ? (
+                  <ConfirmationButton
+                    confirmationText="Tem certeza que deseja apagar o conteudo?"
+                    functionForExecution={()=>{
+                      handleDelete(currentComment._id, currentIndex)
+                    }}
+                  >
+                    Deletar
+                  </ConfirmationButton>
+                ) : null}
               </>
             );
           })}
